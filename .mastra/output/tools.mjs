85311b61f3c0b810e54ffdfd64992641018b370b
shivasaiai <nadigaddashiva5@gmail.com>
2026-05-12 13:55:59 +0530

    chore: stop tracking ignored files

--- .mastra/output/tools.mjs
+++
@@ -1,4 +0,0 @@
-import * as tool0 from './tools/ca1a1ccc-3a38-4847-bc22-6c03244e2041.mjs';
-import * as tool1 from './tools/7e2024f0-8db6-4efe-b204-b846c04beaf3.mjs';
-
-                export const tools = [tool0, tool1]
